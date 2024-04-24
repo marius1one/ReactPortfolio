b7c849fb6103e8b02128c767200b356c200e59fe
DESKTOP-AF5QAEV\ACER <marius.petrulis1@gmail.com>
2024-04-24 20:42:58 +0300

    Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Layout from './components/Layout/index.jsx';
+import './App.scss'
+import {Route, Routes} from "react-router-dom";
+import Home from "./components/Home/index.jsx";
+
+
+function App() {
+
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route index element={<Home />} />
+            </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
