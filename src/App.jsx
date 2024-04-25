0621256cfcdc66be9bc2977faef593f3b04757d5
DESKTOP-AF5QAEV\ACER <marius.petrulis1@gmail.com>
2024-04-25 18:12:26 +0300

    Added contact page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.scss'
 import {Route, Routes} from "react-router-dom";
 import Home from "./components/Home/index.jsx";
 import About from "./components/About/index.jsx";
+import Contact from "./components/Contact/index.jsx";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Layout />} >
           <Route index element={<Home />} />
             <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
             </Route>
       </Routes>
     </>
